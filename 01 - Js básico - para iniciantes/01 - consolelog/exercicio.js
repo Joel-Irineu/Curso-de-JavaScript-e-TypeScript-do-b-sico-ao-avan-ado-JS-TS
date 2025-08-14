754c08af79e88bd88769e7b3e973b3ca2d1b6f77
Joel-Irineu <irineu.joel01@gmail.com>
2025-08-14 12:09:24 -0300

    exercicio console

---
+++ 01 - Js básico - para iniciantes/01 - consolelog/exercicio.js
@@ -0,0 +1,4 @@
+// você deve exibir no console a seguinte mensagem: Meu nomé "NOME". Estou aprendendo Javascript as 10 da manhã.
+// o numero da hora exibido na frase deve ser do tipo number inteiro.
+console.log('Meu nome é "Joel". Estou aprendendo Javascript às', 10, 'da manhã.');
+console.log(`Meu nome é "Joel". Estou aprendendo Javascript às ${10} da manhã.`);
