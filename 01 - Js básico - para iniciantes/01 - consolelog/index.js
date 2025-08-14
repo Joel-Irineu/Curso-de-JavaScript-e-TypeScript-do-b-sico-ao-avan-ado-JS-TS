8fd0ce48733a4d7addec541b5a1d01f38f4c78fe
Joel-Irineu <irineu.joel01@gmail.com>
2025-08-14 12:04:39 -0300

    aula console.log

---
+++ 01 - Js básico - para iniciantes/01 - consolelog/index.js
@@ -0,0 +1,8 @@
+// imprime uma string no console; string = caracteres entre aspas
+console.log('Hello, World!');
+console.log("Olá, mundo!");
+console.log(`Olá, mundo!`);
+
+// imprime um número no console; números podem ser inteiros ou decimais
+console.log(42);
+console.log(3.14);
