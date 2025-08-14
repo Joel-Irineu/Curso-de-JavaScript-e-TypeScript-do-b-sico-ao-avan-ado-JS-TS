afbbbbfeb3f5487bb77f25b9be2a45c4b55db60a
Joel-Irineu <irineu.joel01@gmail.com>
2025-08-14 12:13:37 -0300

    aula sobre comentarios

---
+++ 01 - Js básico - para iniciantes/02 - comentarios/index.js
@@ -0,0 +1,10 @@
+// comentarios são trechos de código que são ignorados pelo JavaScript
+// Comentários de uma linha
+// console.log('Olá, mundo!'); // Esta linha não será executada
+console.log('Este código será executado'); // Esta linha será executada
+/*
+Comentários de múltiplas linhas
+console.log('Esta linha será executada');
+console.log('Esta também');
+*/
+
