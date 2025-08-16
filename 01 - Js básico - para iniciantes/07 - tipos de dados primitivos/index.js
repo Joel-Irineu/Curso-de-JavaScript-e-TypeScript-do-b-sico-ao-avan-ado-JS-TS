f47536888c97e190ecbaf6d6a863a01cec2e6b6f
Joel-Irineu <irineu.joel01@gmail.com>
2025-08-15 21:32:29 -0300

    tipos primitivos

---
+++ 01 - Js básico - para iniciantes/07 - tipos de dados primitivos/index.js
@@ -0,0 +1,16 @@
+const nome = 'Luiz'; // string
+const nome1 = "Luiz"; // string
+const nome2 = `Luiz`; // string
+const num1 = 10; // number
+const num2 = 10.52; // number 
+let nomeAluno; // undefined (indefinido) -> não aponta pra local nenhum na memória
+let sobrenomeAluno = null; // null (nulo) -> tbm não aponta, mas é um valor explicito
+const aprovado = true; // Boolean -> recebe true ou false (verdadeiro ou falso)
+
+
+console.log(typeof nome, nome); // typeof + variavel retorna o tipo da variavel
+console.log(typeof num1, num1);
+console.log(typeof nomeAluno, nomeAluno);
+console.log(typeof sobrenomeAluno, sobrenomeAluno);
+console.log(typeof aprovado, aprovado);
+
