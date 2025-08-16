35229d8fe39e1297690e30af1ff81dc376938a57
Joel-Irineu <irineu.joel01@gmail.com>
2025-08-15 22:48:43 -0300

    trabalhando com metodos do navegador, prompt, alert e confirm

---
+++ 01 - Js básico - para iniciantes/09 - alert, confirm e prompt(navegador)/js/scripts.js
@@ -0,0 +1,5 @@
+alert('Com a mesagem'); // caixa de mensagem
+confirm('Deseja memsmo deletar?'); // caixa de confirmação
+prompt('digite o seu nome'); // caixa de input
+
+// tudo retornado pelo prompt é uma string
