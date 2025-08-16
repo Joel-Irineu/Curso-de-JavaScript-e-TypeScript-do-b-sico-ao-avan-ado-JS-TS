6e9a8bc11e1eae572a767dc9167407cd9e7a83ee
Joel-Irineu <irineu.joel01@gmail.com>
2025-08-15 22:53:02 -0300

    Exercicio soma no prompt

---
+++ 01 - Js básico - para iniciantes/09 - alert, confirm e prompt(navegador)/js/exercicio.js
@@ -0,0 +1,6 @@
+// Objetivo: somar 2 numeros informados pelo prompt
+
+let num1 = prompt('Digite o primeiro numero:');
+let num2 = prompt('Digite o segundo numero:');
+
+alert(`O resultado da soma de ${num1} e ${num2} é: ${Number(num1) + Number(num2)}`);
